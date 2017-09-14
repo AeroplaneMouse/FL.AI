7277e65419f2a4ba2051d3dc7b53764414fabbc0
Daniel Fly <danielfly42@gmail.com>
2017-09-14 17:16:38 +0200

    Added distance sensor support

--- FL.AI/FL.AI.ino
+++ FL.AI/FL.AI.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+const int trig = 2;
+const int echo = 3;
 
+void setup() {
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.print(getDistance());
+  Serial.println(" cm");
+  delay(100);
 }
+
+int getDistance(){
+  // Gets the distance from the sensor in cm.
+  digitalWrite(trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig, LOW);
+  long duration = pulseIn(3, HIGH, 90000);
+  long distance = (duration/2) / 29.1;
+  return distance;
+}
+
