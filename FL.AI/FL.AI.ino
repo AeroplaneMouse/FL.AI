21be7aec5f1f341034314f7b5b84a64cf8cd8caf
Daniel Fly <danielfly42@gmail.com>
2017-09-19 14:08:21 +0200

    Added DC motor control

--- FL.AI/FL.AI.ino
+++ FL.AI/FL.AI.ino
@@ -1,16 +1,47 @@
 const int trig = 2;
 const int echo = 3;
 
+const int f1 = 9;
+const int b1 = 10;
+const int f2 = 5;
+const int b2 = 6;
+
+int counter = 0;
+int i = 0;
+
 void setup() {
   pinMode(trig, OUTPUT);
   pinMode(echo, INPUT);
+  
+  pinMode(f1, OUTPUT);
+  pinMode(b1, OUTPUT);
+  pinMode(f2, OUTPUT);
+  pinMode(b2, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print(getDistance());
-  Serial.println(" cm");
-  delay(100);
+  int distance = getDistance();
+  //Serial.print(distance);
+  //Serial.println(" cm");
+
+  if (distance < 20){
+    /*
+    if (counter == 10){
+      forward(i);
+      i += 50;
+    }
+    if (i > 255) i = 0;
+    */
+    forward(0);
+  }
+  else{
+    forward(255);
+  }
+  
+  if (counter == 10) counter = 0;
+  counter++;
+  delay(10);
 }
 
 int getDistance(){
@@ -23,3 +54,28 @@ int getDistance(){
   return distance;
 }
 
+void backward(int motor, int pwm){
+  if (motor == 1){
+    digitalWrite(f1, LOW);
+    delay(200);
+    analogWrite(b1, pwm);
+  }
+  else{
+    digitalWrite(f2, LOW);
+    delay(200);
+    analogWrite(b2, pwm);
+  }
+}
+
+void forward(int motor, int pwm){
+  if (motor  == 1){
+    digitalWrite(b1, LOW);
+    delay(200);
+    analogWrite(f1, pwm);
+  }
+  else{
+    digitalWrite(b2, LOW);
+    delay(200);
+    analogWrite(f2, pwm);
+  }
+}
